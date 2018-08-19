98e61750d74d2ed883d83ba3664a5dbe499cf4c2
Little_guang <m26402000@gmail.com>
2018-08-19 20:28:06 +0800

    virtual

--- catkin_ws/build/image_recog/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ball/catkin_ws/src/image_recog
-# Build directory: /home/ball/catkin_ws/build/image_recog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
